27d80967529412c0ece870ec6dd50493dbd6aa5b
John-Luke Brooks <57413136+Ashtroyd@users.noreply.github.com>
2024-04-30 12:28:18 +0000

    Weight

--- The Fitness Pros 0.00.00.1/obj/Debug/net8.0/The Fitness Pros 0.00.00.1.AssemblyInfo.cs
+++ The Fitness Pros 0.00.00.1/obj/Debug/net8.0/The Fitness Pros 0.00.00.1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("The_Fitness_Pros_0.00.00.1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86ecbbdd4c1a7c6adebb94909dea2aa3c560e12f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf7a2f6457d43fbf328ba7e2b42719e388b88ae8")]
 [assembly: System.Reflection.AssemblyProductAttribute("The_Fitness_Pros_0.00.00.1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("The_Fitness_Pros_0.00.00.1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
