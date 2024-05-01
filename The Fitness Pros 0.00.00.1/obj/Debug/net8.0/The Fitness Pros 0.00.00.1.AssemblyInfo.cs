1d317c72d8158343fd444dde06b16202eff12b4a
CallumBelfield <callum.belfield.main@gmail.com>
2024-05-01 14:46:05 +0100

    Changed navigation on health page

--- The Fitness Pros 0.00.00.1/obj/Debug/net8.0/The Fitness Pros 0.00.00.1.AssemblyInfo.cs
+++ The Fitness Pros 0.00.00.1/obj/Debug/net8.0/The Fitness Pros 0.00.00.1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("The_Fitness_Pros_0.00.00.1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc054b3a006e565abed234b383a2c2fc27a04df8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5c6c0b325d5a1b811710a20b50c70093fb017af")]
 [assembly: System.Reflection.AssemblyProductAttribute("The_Fitness_Pros_0.00.00.1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("The_Fitness_Pros_0.00.00.1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
