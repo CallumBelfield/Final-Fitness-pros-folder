e698227b9a39e9b9061d9c05f5f634c31abe3f87
CallumBelfield <callum.belfield.main@gmail.com>
2024-05-01 15:11:51 +0100

    Tinkering with font and sizes to be visibly appealing on mobile.

--- The Fitness Pros 0.00.00.1/obj/Debug/net8.0/The Fitness Pros 0.00.00.1.AssemblyInfo.cs
+++ The Fitness Pros 0.00.00.1/obj/Debug/net8.0/The Fitness Pros 0.00.00.1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("The_Fitness_Pros_0.00.00.1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d317c72d8158343fd444dde06b16202eff12b4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13c1a9d95742527300ec89e376b3f52d5715d405")]
 [assembly: System.Reflection.AssemblyProductAttribute("The_Fitness_Pros_0.00.00.1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("The_Fitness_Pros_0.00.00.1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
