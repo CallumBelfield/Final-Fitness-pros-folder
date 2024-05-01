9b8f9acf15dc014e8d0f4857266f85e8261dd0c4
John-Luke Brooks <57413136+Ashtroyd@users.noreply.github.com>
2024-05-01 13:37:12 +0000

    Centering Text

--- The Fitness Pros 0.00.00.1/obj/Debug/net8.0/The Fitness Pros 0.00.00.1.AssemblyInfo.cs
+++ The Fitness Pros 0.00.00.1/obj/Debug/net8.0/The Fitness Pros 0.00.00.1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("The_Fitness_Pros_0.00.00.1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+883470ced2690674e7bff84bfd5c6ccc0175af83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa76841ee2534776d10578097f8923861d586580")]
 [assembly: System.Reflection.AssemblyProductAttribute("The_Fitness_Pros_0.00.00.1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("The_Fitness_Pros_0.00.00.1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
